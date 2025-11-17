ed2a42511b4d16e737ef7545495f624c0b2bb34d
rahuls316 <153314591+rahuls316@users.noreply.github.com>
2025-11-17 13:24:10 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,13 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '*',
-      disallow: '',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://www.webincoder.com/sitemap.xml',
   }
 }
