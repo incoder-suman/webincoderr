cddb1e878c930a615a1b4dd2a098f3d43dd85ef9
Suman Kumar <sumanking467@gmail.com>
2025-11-18 10:42:57 +0530

    layout js update

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "/logo.png", // 👈 This will appear when shared on WhatsApp, LinkedIn etc.
+        url: "/logo.png",
         width: 512,
         height: 512,
         alt: "Webincoder Logo",
@@ -38,49 +38,47 @@ export const metadata = {
   },
 
   icons: {
-    icon: "/logo.png",      // 👈 main favicon (browser tab)
-    shortcut: "/logo.png",  // 👈 quick icon for Chrome/Edge tabs
-    apple: "/logo.png",     // 👈 for iPhones & iPads
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
   },
 
-  manifest: "/site.webmanifest", // optional but good for PWA/future
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-    <head>
-    // Google Tag Manager
-<script id="gtm-head" strategy="beforeInteractive">
-  {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NKVDFXWD');
-`}
-</script>
- // End Google Tag Manager 
-  </head>
-    
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NKVDFXWD');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F8FAFC] dark:bg-[#0F172A] text-[#1E293B] dark:text-[#E2E8F0] transition-colors duration-300`}
       >
- // Google Tag Manager (noscript)
-<noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKVDFXWD"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-  </noscript>
-// End Google Tag Manager (noscript)
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NKVDFXWD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
-  
-        {/* Navbar */}
         <Header />
-
-        {/* Page content */}
         <main className="pt-20">{children}</main>
-
-        {/* Footer */}
         <Footer />
       </body>
     </html>
