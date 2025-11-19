18ba78d3bf13e8c4971df308f160c951b09c33b3
rahuls316 <153314591+rahuls316@users.noreply.github.com>
2025-11-19 17:40:16 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,6 @@ import Script from "next/script";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -56,6 +46,16 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 
 export const metadata = {
